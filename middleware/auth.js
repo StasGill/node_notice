28e3f9f40c070d508f64e43422a3fd6e18cf8f79
Stanislav Gil <stasgill@gmail.com>
2022-05-17 20:22:55 +0300

    fix bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken";
 
 const auth = (req, res, next) => {
-  // console.log(req.headers.authorization);
   try {
-    const token = req.headers?.authorization?.split(" ")[1];
+    const token =
+      req.headers.authorization && req.headers?.authorization?.split(" ")[1];
 
     const isCustomAuth = token.length < 500;
 
